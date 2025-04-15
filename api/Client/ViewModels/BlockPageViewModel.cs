feab5225c9a22a4f4fd9469ba3e2e17414296752
Howaitoniksu <129360255+Howaitoniksu@users.noreply.github.com>
2025-04-16 00:34:08 +0300

    #

--- api/Client/ViewModels/BlockPageViewModel.cs
+++ api/Client/ViewModels/BlockPageViewModel.cs
@@ -1,10 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Avalonia.Interactivity;
 using Client.Models.Blocks;
 using Client.Models.Courses;
 using Newtonsoft.Json;
 using ReactiveUI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Client.ViewModels
 {
@@ -20,9 +20,12 @@ namespace Client.ViewModels
             GetData(currentCourse);
         }
 
+        private bool myPopup = false;
+
         private List<ShortBlockDTO> _blocks = new();
 
         public List<ShortBlockDTO> Blocks { get => _blocks; set => this.RaiseAndSetIfChanged(ref _blocks, value); }
+        public bool MyPopup { get => myPopup; set => this.RaiseAndSetIfChanged(ref myPopup, value); }
 
         async Task GetData(ShortCourseDTO currentCourse)
         {
@@ -30,6 +33,15 @@ namespace Client.ViewModels
             Blocks = JsonConvert.DeserializeObject<List<ShortBlockDTO>>(response);
         }
 
+        private void OnButtonClick(object? sender, RoutedEventArgs e)
+        {
+            MyPopup = true;
+        }
+
+        private void OnClosePopupClick(object? sender, RoutedEventArgs e)
+        {
+            MyPopup = false;
+        }
 
     }
 }
