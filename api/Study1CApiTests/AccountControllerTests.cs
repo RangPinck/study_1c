28462c980d8622fe278bcb011ce09eb1607f7518
Bax <chernoff.serg3i@yandex.ru>
2025-04-17 02:53:50 +0300

    feat: start tesing

---
+++ api/Study1CApiTests/AccountControllerTests.cs
@@ -0,0 +1,141 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Study1CApi.Controllers;
+using Study1CApi.DTOs.AuthDTO;
+using Study1CApi.Interfaces;
+using Study1CApi.Models;
+using Study1CApi.Repositories;
+
+namespace Study1CApiTests
+{
+    public class AccountControllerTests
+    {
+        private readonly ITokenService _tokenService;
+        private readonly Study1cDbContext _context;
+        private Guid idProfile;
+
+        public AccountControllerTests()
+        {
+            var config = new Mock<IConfiguration>();
+            var configSectionMock = new Mock<IConfigurationSection>();
+            config.Setup(conf => conf["JWT:SigningKey"]).Returns("s3cr3t!k3y[f0r=study.1C.jwt.auth.educ@t10n4kjeh37i4fhwifh9-wfj;2ojq3f09q2fjhoidfgh8723tfiwkfgs8w7fsf");
+            configSectionMock.Setup(x => x.Value).Returns("40.0");
+            config.Setup(conf => conf.GetSection("JWT:TokenValidMinutes"))
+                .Returns(configSectionMock.Object);
+            config.Setup(conf => conf["JWT:Issuer"]).Returns("https://localhost:7053");
+            config.Setup(conf => conf["JWT:Audience"]).Returns("study.1C.client");
+            _tokenService = new TokenService(config.Object);
+            _context = GetDatabaseContext().Result;
+        }
+
+        private async Task<Study1cDbContext> GetDatabaseContext()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var options = new DbContextOptionsBuilder<Study1cDbContext>()
+                .UseInMemoryDatabase("TestDatabase")
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+            var databaseContext = new Study1cDbContext(options);
+            databaseContext.Database.EnsureCreated();
+
+            if (await databaseContext.Users.CountAsync() <= 0)
+            {
+                idProfile = Guid.NewGuid();
+                databaseContext.Users.Add(
+                    new User()
+                    {
+                        UserId = idProfile,
+                        UserSurname = "admin",
+                        UserName = "admin",
+                        UserPatronymic = "",
+                        AuthUserNavigation = new AuthUser()
+                        {
+                            Id = idProfile,
+                            Email = "admin@admin.com",
+                        }
+                    });
+                databaseContext.UserRoles.Add(new IdentityUserRole<Guid>() { RoleId = new Guid("f45d2396-3e72-4ec7-b892-7bd454248158"), UserId = idProfile });
+                await databaseContext.SaveChangesAsync();
+
+            }
+            return databaseContext;
+        }
+
+        [Fact]
+        public async Task AccountControllerTests_Login_ReturnOKAsyncAndToken()
+        {
+            var dto = new LoginDTO()
+            {
+                Email = "admin@admin.com",
+                Password = "admin1cdbapi"
+            };
+
+            var userManagerMock = new Mock<UserManager<AuthUser>>(
+                new Mock<IUserStore<AuthUser>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<AuthUser>>().Object,
+                new IUserValidator<AuthUser>[0],
+                new IPasswordValidator<AuthUser>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<AuthUser>>>().Object);
+            userManagerMock
+                .Setup(userManager => userManager
+                .CreateAsync(It.IsAny<AuthUser>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(IdentityResult.Success));
+            userManagerMock
+                .Setup(um => um.AddToRoleAsync(It.IsAny<AuthUser>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(IdentityResult.Success));
+            userManagerMock
+                .Setup(um => um.FindByEmailAsync(dto.Email)).
+                ReturnsAsync(new AuthUser { UserName = dto.Email, Email = dto.Email, Id = idProfile });
+            userManagerMock
+                .Setup(um => um.GetRolesAsync(It.Is<AuthUser>(u => u.Id == idProfile)))
+                .ReturnsAsync(new List<string> { "Администратор" });
+
+            var signInManager = new Mock<SignInManager<AuthUser>>(
+                userManagerMock.Object, Mock.Of<IHttpContextAccessor>(),
+                Mock.Of<IUserClaimsPrincipalFactory<AuthUser>>(), null, null, null, null);
+
+            signInManager
+                .Setup(s => s.CheckPasswordSignInAsync(It.IsAny<AuthUser>(), dto.Password, false))
+                .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
+            var roleStoreMock = new Mock<IRoleStore<Role>>();
+
+            var mockRoleManager = new Mock<RoleManager<Role>>(
+                roleStoreMock.Object,
+                null,
+                null,
+                null,
+                null 
+            );
+
+            var accountRepository = new AccountRepository(_context);
+
+            var controller = new AccountController(userManagerMock.Object, _tokenService, signInManager.Object, mockRoleManager.Object, _context, accountRepository);
+
+            var result = await controller.Login(dto);
+
+            if (result is OkObjectResult okResult)
+            {
+                var resultDto = (SignInDTO)okResult.Value;
+                Assert.NotNull(resultDto.Token);
+            }
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<OkObjectResult>();
+        }
+    }
+}
