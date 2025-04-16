cd4dcdd9cb75954ba3246b363464a9f0c9edfc24
Bax <chernoff.serg3i@yandex.ru>
2025-04-16 12:48:18 +0300

    feat: add statistic for user

--- api/Study1CApi/Repositories/UserRepository.cs
+++ api/Study1CApi/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@ using Study1CApi.Models;
 using Study1CApi.DTOs.UserDTOs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Study1CApi.DTOs.BlockDTOs;
 
 namespace Study1CApi.Repositories
 {
@@ -21,9 +22,11 @@ namespace Study1CApi.Repositories
         {
             try
             {
-                var profiles = await _context.Users.AsNoTracking().ToListAsync();
                 var users = await _userManager.Users.AsNoTracking().ToListAsync();
+                var profiles = await _context.Users.Include(x => x.UserCourses).Include(x => x.UsersTasks).AsNoTracking().ToListAsync();
+
                 List<UserDTO> result = new List<UserDTO>();
+
                 if (isAdmin)
                 {
                     result = users.Select(user => new UserDTO()
@@ -35,6 +38,22 @@ namespace Study1CApi.Repositories
                         UserPatronymic = profiles.FirstOrDefault(x => x.UserId == user.Id).UserPatronymic,
                         UserDataCreate = user.UserDataCreate,
                         UserRole = _userManager.GetRolesAsync(user).Result.ToList(),
+                        UserStatistics = _context.CoursesBlocks.Select(block => new BlockStatisticsDTO()
+                        {
+                            BlockId = block.BlockId,
+
+                            FullyCountTask = block.BlocksTasks.Count() + block.BlocksMaterials.Where(mat => mat.Duration != null).Count() + block.BlocksTasks.Select(x => x.TasksPractices.Count()).Sum(),
+
+                            FullyDurationNeeded = block.BlocksTasks.Select(x => x.Duration).Sum() + block.BlocksMaterials.Where(mat => mat.Duration != null).Select(x => Convert.ToInt32(x.Duration)).Sum() + block.BlocksTasks.Select(x => x.TasksPractices.Select(x => Convert.ToInt32(x.Duration)).Sum()).Sum(),
+
+                            CompletedTaskCount = _context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Count(),
+
+                            DurationCompletedTask = _context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Sum(x => x.DurationMaterial + x.DurationPractice + x.DurationTask),
+
+                            PercentCompletedTask = Math.Round((double)_context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Count() / (double)(block.BlocksTasks.Count() + block.BlocksMaterials.Where(mat => mat.Duration != null).Count() + block.BlocksTasks.Select(x => x.TasksPractices.Count()).Sum()) * 100.0, 2),
+
+                            PercentDurationCompletedTask = Math.Round((double)_context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Sum(x => x.DurationMaterial + x.DurationPractice + x.DurationTask) / (double)(block.BlocksTasks.Select(x => x.Duration).Sum() + block.BlocksMaterials.Where(mat => mat.Duration != null).Select(x => Convert.ToInt32(x.Duration)).Sum() + block.BlocksTasks.Select(x => x.TasksPractices.Select(x => Convert.ToInt32(x.Duration)).Sum()).Sum()) * 100.0, 2),
+                        }).ToList()
                     }).ToList();
                 }
                 else
@@ -48,6 +67,22 @@ namespace Study1CApi.Repositories
                         UserPatronymic = profiles.FirstOrDefault(x => x.UserId == user.Id).UserPatronymic,
                         UserDataCreate = user.UserDataCreate,
                         UserRole = _userManager.GetRolesAsync(user).Result.ToList(),
+                        UserStatistics = _context.CoursesBlocks.Select(block => new BlockStatisticsDTO()
+                        {
+                            BlockId = block.BlockId,
+
+                            FullyCountTask = block.BlocksTasks.Count() + block.BlocksMaterials.Where(mat => mat.Duration != null).Count() + block.BlocksTasks.Select(x => x.TasksPractices.Count()).Sum(),
+
+                            FullyDurationNeeded = block.BlocksTasks.Select(x => x.Duration).Sum() + block.BlocksMaterials.Where(mat => mat.Duration != null).Select(x => Convert.ToInt32(x.Duration)).Sum() + block.BlocksTasks.Select(x => x.TasksPractices.Select(x => Convert.ToInt32(x.Duration)).Sum()).Sum(),
+
+                            CompletedTaskCount = _context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Count(),
+
+                            DurationCompletedTask = _context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Sum(x => x.DurationMaterial + x.DurationPractice + x.DurationTask),
+
+                            PercentCompletedTask = Math.Round((double)_context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Count() / (double)(block.BlocksTasks.Count() + block.BlocksMaterials.Where(mat => mat.Duration != null).Count() + block.BlocksTasks.Select(x => x.TasksPractices.Count()).Sum()) * 100.0, 2),
+
+                            PercentDurationCompletedTask = Math.Round((double)_context.UsersTasks.Where(x => x.AuthUser == user.Id && x.Status == 3 && (x.MaterialNavigation.Block == block.BlockId || x.TaskNavigation.Block == block.BlockId || x.PracticeNavigation.TaskNavigation.Block == block.BlockId)).Sum(x => x.DurationMaterial + x.DurationPractice + x.DurationTask) / (double)(block.BlocksTasks.Select(x => x.Duration).Sum() + block.BlocksMaterials.Where(mat => mat.Duration != null).Select(x => Convert.ToInt32(x.Duration)).Sum() + block.BlocksTasks.Select(x => x.TasksPractices.Select(x => Convert.ToInt32(x.Duration)).Sum()).Sum()) * 100.0, 2),
+                        }).ToList()
                     }).Where(x => x.UserRole.Contains("Ученик")).ToList();
                 }
 
