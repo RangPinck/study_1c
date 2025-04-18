3c2a9be5d6677e1509c37076ed62ec30e1aee002
Bax <chernoff.serg3i@yandex.ru>
2025-04-18 04:21:28 +0300

    fix: fix bugs

--- api/Client/ViewModels/AuthPageViewModel.cs
+++ api/Client/ViewModels/AuthPageViewModel.cs
@@ -7,9 +7,9 @@ namespace Client.ViewModels
 	public class AuthPageViewModel : ViewModelBase
 	{
 
-		private string _email = "admin@admin.com";
+		private string _email = string.Empty;
 
-		private string _password = "admin1cdbapi";
+		private string _password = string.Empty;
 
         public string Email { get => _email; set => this.RaiseAndSetIfChanged(ref _email , value); }
         public string Password { get => _password; set => this.RaiseAndSetIfChanged(ref _password, value); }
